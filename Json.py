b650955b13763e403a55b945276750a5a600449a
Tokariev <mykola.tokariev@gmail.com>
2020-01-21 20:18:02 +0100

    Add logic for So - Sa days

--- Json.py
+++ Json.py
@@ -4,9 +4,6 @@ class Json:
     def string_to_json(self, s):
         return json.loads(s)
 
-    def get_days_from_json(self, json):
-        return map(lambda datum: datum['name'], json)
-
     def get_day_number(self, day):
         weeks_day = {
             "Mo": 1,
@@ -51,14 +48,27 @@ class Json:
             if '-' in day:
                 firs_day = day[:2]
                 last_day = day[-2:]
+
                 firs_day_numb = self.get_day_number(firs_day)
                 last_day_numb = self.get_day_number(last_day)
+                #if Sa - So
+                if firs_day_numb - last_day_numb > 0:
+                    days_range_1 = range(1, last_day_numb + 1, 1)
+                    dyys_range_2 = range(firs_day_numb, 7 + 1, 1)
 
-                days_range = range(firs_day_numb, last_day_numb + 1, 1)
+                    for day_numb in days_range_1:
+                        day = self.get_key_from_value(day_numb)
+                        days_list.append(self.parse_time(day, time))
 
-                for day_numb in days_range:
-                    day = self.get_key_from_value(day_numb)
-                    days_list.append(self.parse_time(day, time))
+                    for day_numb in dyys_range_2:
+                        day = self.get_key_from_value(day_numb)
+                        days_list.append(self.parse_time(day, time))
+                else:
+                    days_range = range(firs_day_numb, last_day_numb + 1, 1)
+
+                    for day_numb in days_range:
+                        day = self.get_key_from_value(day_numb)
+                        days_list.append(self.parse_time(day, time))
 
             elif '-' not in day:
                 days_list.append(self.parse_time(day, time))
